7246e6e812ed7341b84b581e590c212dcf865a79
jens426 <jepou426@gmail.com>
2023-03-19 16:10:49 +0100

    pretty fied code

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,51 +1,67 @@
- 
-    function sayHelloWorld() {
-        console.log('hello world');
-        return null;
-    }
-/*
-    function drawNext() {
-        var c = document.getElementById("myCanvas");
-        var ctx = c.getContext("2d");
-        ctx.beginPath();
-        ctx.rect(50, 50, 100, 100);
-        ctx.stroke();
-    }
-    */
+function sayHelloWorld() {
+  console.log("hello world");
+  return null;
+}
 
 // drawnall
-    function drawEverThing(){
-     //drawNext();
-     var c = document.getElementById("myCanvas");
-     var allTilesArray = new Array();
+function drawEverThing() {
+  //drawNext();
+  var c = document.getElementById("myCanvas");
+  var allTilesArray = new Array();
+  const name = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+  ];
 
-     var sizeb = 0;
-     for(var ii = 1; ii < 17; ii++){
-        var size = 0;
-        for(var i = 1; i < 10; i++){
-            allTilesArray.push(new Tile(350+size,150+sizeb,35,35,"a2",c));
-            size = size+35;
-        }
-        var size = 0;
-        var sizeb = sizeb+35;
+  var sizeb = 0;
+  var nextNameInArray = 0;
+  for (var ii = 1; ii < 17; ii++) {
+    var size = 0;
+    for (var i = 1; i < 10; i++) {
+      allTilesArray.push(
+        new Tile(
+          350 + size,
+          150 + sizeb,
+          35,
+          35,
+          name[nextNameInArray] + "" + i,
+          c
+        )
+      );
+      size = size + 35;
     }
+    nextNameInArray = nextNameInArray + 1;
+    sizeb = sizeb + 35;
+  }
 
-  
-     for (const element of allTilesArray) {
-        element.draw()
-      }
+  for (const element of allTilesArray) {
+    element.draw();
+  }
 
-     //a1.draw();
-     //console.log(a1.getName()); 
-
-
-    }
-        // import of class is done in html
-     
+  // print all name of tiels
+  /*
+  for (const element of allTilesArray) {
+    console.log(element.getName());
+  }
+  */
+}
+// import of class is done in html
 
 drawEverThing();
 
-
-
-console.log('hello world');
-console.log('this is line 2');
+console.log("hello world");
+console.log("this is line 2");
--- Tile.js
+++ Tile.js
@@ -1,31 +1,29 @@
 class Tile {
-    constructor(x,y,x2,y2,name,canvas) {
-        this.x = x;
-        this.y = y;
-        this.x2 = x2;
-        this.y2 = y2;
-        this.name = name;
-        this.canvas = canvas;
-    }
-
-    getName() {
-        return this.name;
-      }
-
-      draw() {
-        var ctx = this.canvas.getContext("2d");
-        ctx.beginPath();
-        ctx.rect(this.x, this.y, this.x2, this.y2);
-        ctx.stroke();
-    }
-
-    printAll(){
-        console.log(this.x);
-        console.log(this.y);
-        console.log(this.x+this.size);
-        console.log(this.y+this.size);
-        console.log(this.size);
-    }
-
-
+  constructor(x, y, x2, y2, name, canvas) {
+    this.x = x;
+    this.y = y;
+    this.x2 = x2;
+    this.y2 = y2;
+    this.name = name;
+    this.canvas = canvas;
   }
+
+  getName() {
+    return this.name;
+  }
+
+  draw() {
+    var ctx = this.canvas.getContext("2d");
+    ctx.beginPath();
+    ctx.rect(this.x, this.y, this.x2, this.y2);
+    ctx.stroke();
+  }
+
+  printAll() {
+    console.log(this.x);
+    console.log(this.y);
+    console.log(this.x + this.size);
+    console.log(this.y + this.size);
+    console.log(this.size);
+  }
+}
