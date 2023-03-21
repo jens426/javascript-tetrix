d35be3f5379a31406d20b03f7ef74a1adef0af7d
jens426 <jepou426@gmail.com>
2023-03-21 12:44:03 +0100

    colition test with array of point to test, instead of hell with if statement that will become hell when all 7 pieces and there collition have to be program with if statements.

--- Piece.js
+++ Piece.js
@@ -1,5 +1,12 @@
 class Piece {
-  position = [4, 5, 14, 15];
+  TileFourPosition = [4, 5, 14, 15];
+
+  // 1 is left side that has to be check for colliton
+  TileFourLeftSide = [1, 0, 1, 0];
+
+  TileFourRightSide = [0, 1, 0, 1];
+  TileFourBottom = [0, 0, 1, 1];
+
   stopMoving = false;
   constructor(color, shape, allTiles) {
     this.color = color;
@@ -13,7 +20,7 @@ class Piece {
 
   placePiece() {
     if (this.shape == 4) {
-      for (const element of this.position) {
+      for (const element of this.TileFourPosition) {
         var temp = this.allTiles[element];
         this.pieceFour(temp);
 
@@ -29,21 +36,21 @@ class Piece {
   }
 
   moveFourDown() {
-    this.collitionDetectionFour();
+    this.collitionDetectionDown(this.TileFourPosition, this.TileFourBottom);
 
     if (this.stopMoving == false) {
-      for (const element of this.position) {
+      for (const element of this.TileFourPosition) {
         var temp = this.allTiles[element];
         this.pieceFour(temp);
         temp.setEmpty();
       }
-      this.position = [
-        this.position[0] + 10,
-        this.position[1] + 10,
-        this.position[2] + 10,
-        this.position[3] + 10,
+      this.TileFourPosition = [
+        this.TileFourPosition[0] + 10,
+        this.TileFourPosition[1] + 10,
+        this.TileFourPosition[2] + 10,
+        this.TileFourPosition[3] + 10,
       ];
-      for (const element of this.position) {
+      for (const element of this.TileFourPosition) {
         var temp = this.allTiles[element];
         this.pieceFour(temp);
         temp.setPiece(this.color);
@@ -52,90 +59,162 @@ class Piece {
   }
 
   moveFourleft() {
-    if (this.stopMoving == false) {
-      if (this.allTiles[this.position[2]].getLeftTile() != null) {
-        // wall hit
-        if (
-          this.allTiles[this.position[2]].getLeftTile().getIsSet() == false &&
-          this.allTiles[this.position[0]].getLeftTile().getIsSet() == false
-        ) {
-          // hit a other pice to left
+    this.moveLeft(this.TileFourPosition, this.TileFourLeftSide);
+  }
 
-          for (const element of this.position) {
-            var temp = this.allTiles[element];
-            this.pieceFour(temp);
-            temp.setEmpty();
+  moveRight(TilePosition, TileRight) {
+
+    if (this.stopMoving == false) {
+      let isAllowToMove = true;
+
+      let ArrayToCheck = [];
+      for (let i = 0; i < TileRight.length; i++) {
+        if (TileRight[i] == 0) {
+          ArrayToCheck.push(0);
+        } else {
+          ArrayToCheck.push(TilePosition[i]);
+        }
+      }
+
+      for (const element of ArrayToCheck) {
+        if (element == 0) {
+          // do nothing
+        } else {
+          if (this.allTiles[element].getRightTile() == null) {
+            // wall hit
+
+            isAllowToMove = false;
+          } else {
+            if (this.allTiles[element].getRightTile().getIsSet() == true) {
+              isAllowToMove = false;
+            }
           }
-          this.position = [
-            this.position[0] - 1,
-            this.position[1] - 1,
-            this.position[2] - 1,
-            this.position[3] - 1,
-          ];
-          for (const element of this.position) {
-            var temp = this.allTiles[element];
-            this.pieceFour(temp);
-            temp.setPiece(this.color);
-          }
+        }
+      }
+
+      // not hit a other pice to right
+      if (isAllowToMove) {
+        // remove from tiles
+        for (const element of TilePosition) {
+          var temp = this.allTiles[element];
+          this.pieceFour(temp);
+          temp.setEmpty();
+        }
+
+        // move position 1 to right
+        TilePosition;
+        for (let i = 0; i < TilePosition.length; i++) {
+          TilePosition[i] = TilePosition[i] + 1;
+        }
+
+        // paint again to the right
+        for (const element of TilePosition) {
+          var temp = this.allTiles[element];
+          this.pieceFour(temp);
+          temp.setPiece(this.color);
         }
       }
     }
   }
 
+  moveLeft(TilePosition, TileLeft) {
+
+    if (this.stopMoving == false) {
+      let isAllowToMove = true;
+
+      let ArrayToCheck = [];
+      for (let i = 0; i < TileLeft.length; i++) {
+        if (TileLeft[i] == 0) {
+          ArrayToCheck.push(0);
+        } else {
+          ArrayToCheck.push(TilePosition[i]);
+        }
+      }
+
+      for (const element of ArrayToCheck) {
+        if (element == 0) {
+          // do nothing
+        } else {
+          if (this.allTiles[element].getLeftTile() == null) {
+            // wall hit
+
+            isAllowToMove = false;
+          } else {
+            if (this.allTiles[element].getLeftTile().getIsSet() == true) {
+              isAllowToMove = false;
+            }
+          }
+        }
+      }
+
+      // not hit a other pice to left
+      if (isAllowToMove) {
+        // remove from tiles
+        for (const element of TilePosition) {
+          var temp = this.allTiles[element];
+          this.pieceFour(temp);
+          temp.setEmpty();
+        }
+
+        // move position 1 to left
+        TilePosition;
+        for (let i = 0; i < TilePosition.length; i++) {
+          TilePosition[i] = TilePosition[i] - 1;
+        }
+
+        // paint again to the left
+        for (const element of TilePosition) {
+          var temp = this.allTiles[element];
+          this.pieceFour(temp);
+          temp.setPiece(this.color);
+        }
+      }
+    }
+  }
+
+  
   moveFourRight() {
-    if (this.stopMoving == false) {
-      if (this.allTiles[this.position[3]].getRightTile() != null) {
-        // wall hit
-        if (
-          this.allTiles[this.position[3]].getRightTile().getIsSet() == false &&
-          this.allTiles[this.position[1]].getRightTile().getIsSet() == false
-        ) {
-          // hit a other pice to right
+    this.moveRight(this.TileFourPosition, this.TileFourRightSide);
 
-          for (const element of this.position) {
-            var temp = this.allTiles[element];
-            this.pieceFour(temp);
-            temp.setEmpty();
-          }
-          this.position = [
-            this.position[0] + 1,
-            this.position[1] + 1,
-            this.position[2] + 1,
-            this.position[3] + 1,
-          ];
-          for (const element of this.position) {
-            var temp = this.allTiles[element];
-            this.pieceFour(temp);
-            temp.setPiece(this.color);
-          }
+  }
+
+
+
+  collitionDetectionDown(TilePosition, TileBottom) {
+    let ArrayToCheck = [];
+    for (let i = 0; i < TileBottom.length; i++) {
+      if (TileBottom[i] == 0) {
+        ArrayToCheck.push(0);
+      } else {
+        ArrayToCheck.push(TilePosition[i]);
+      }
+    }
+    for (const element of ArrayToCheck) {
+      if (element == 0) {
+        // do nothing
+      } else {
+        if (this.allTiles[element].getTileUnder() == null) {
+          //bottom
+
+          this.stopMoving = true;
+        } else if (this.allTiles[element].getTileUnder().getIsSet() == true) {
+          //other piece collition down
+
+          this.stopMoving = true;
         }
       }
     }
   }
 
-  collitionDetectionFour() {
-    if (this.shape == 4) {
-      if (this.allTiles[this.position[3]].getTileUnder() == null) {
-        //bottom
-        this.stopMoving = true;
-      } else if (
-        this.allTiles[this.position[3]].getTileUnder().getIsSet() == true
-      ) {
-        //other piece collition down
-        this.stopMoving = true;
-      }
-    }
-  }
-
-  rotateFourLeft(position) {
+  rotateFourLeft(TileFourPosition) {
     if (this.stopMoving == false) {
       this.position = position;
     }
   }
 
-  rotateFourRight(position) {
+  rotateFourRight(TileFourPosition) {
     if (this.stopMoving == false) {
-      this.position = position;
+      this.TileFourPosition = TileFourPosition;
     }
   }
 }
